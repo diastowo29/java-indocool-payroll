7d9aca886c76d637917553a9c9d4c7e7edb3ec2b
Diastowo <diastowo@gmail.com>
2023-07-26 14:03:00 +0700

    add sidebar

--- src/main/java/df/project/indocool/ICPayroll/controller/CalculationController.java
+++ src/main/java/df/project/indocool/ICPayroll/controller/CalculationController.java
@@ -1,16 +1,27 @@
 package df.project.indocool.ICPayroll.controller;
 
+import df.project.indocool.ICPayroll.model.DTSSummaryResults;
+import df.project.indocool.ICPayroll.repository.DTSSummaryResultRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @SpringBootApplication
 public class CalculationController {
 
+	@Autowired
+	DTSSummaryResultRepository summaryResult;
+
 	@RequestMapping("/calculation")
-	String loadDtsInput() {
+	String loadDtsInput(Model model) {
+		List<DTSSummaryResults> sums = summaryResult.findAll();
+		model.addAttribute("sums", sums);
 		return "calculation";
 	}
-	
+
 }
